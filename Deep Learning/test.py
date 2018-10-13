f0548620d07df7020eeefdb4ddb07f0609af6566
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2018-10-13 23:03:25 +0500

    1

---
+++ Deep Learning/test.py
@@ -0,0 +1,14 @@
+from keras.datasets import mnist
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+# Traininfg Data
+train_images.shape
+
+len(train_labels)
+
+# Testing Data
+test_images.shape
+
+len(test_labels)
+
+test_labels
